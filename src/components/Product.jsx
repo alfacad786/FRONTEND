bbcb355ba829dba0b5282fa32e84af8536ace219
alfacad786 <alfa_cad786@yahoo.com>
2024-07-16 14:17:48 +0530

    aws s3 creat buckete by input bucket name and buttone

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,74 +5,114 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const product = () => {
-
-  const [formData, setFormData] = useState({
-    projectName: "",
-    discription: "",
-    image: "",
+  const [formData1, setFormData1] = useState({
+    bucketName: "",
   });
+  // const [formData, setFormData] = useState({
+  //   projectName: "",
+  //   discription: "",
+  //   image: "",
+  // });
   // const [image, setImage] = useState(null);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
+  // const handleChange = (e) => {
+  //   setFormData({
+  //     ...formData,
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
+  // ------------------s3-------------
+  const handleChange1 = (e) => {
+    setFormData1({
+      ...formData1,
       [e.target.name]: e.target.value,
     });
   };
-// ------------------image-------------
-//   const handleImageChange = (e) => {
-//     setImage(e.target.files[0]);
-// };
-// ------------------image-------------
-
-
-
-  const handleSubmit = async (e) => {
+  const handleSubmit1 = async (e) => {
     e.preventDefault();
-// ------------------image-------------
 
-    // const formData = new FormData();
-    // formData.append('image', image);
-// ------------------image-------------
-// try {
-//   const res = await axios.post('/upload', formData, {
-
-//       headers: {
-//           'Content-Type': 'multipart/form-data'
-//       }
-//   });
-
-//   console.log(res.data);
-// } catch (err) {
-//   console.error(err);
-// }
-// ------------------image-------------
-
-   axios.post("http://localhost:3000/api/product", formData,)
-      // const data = response.json();
-     .then(response=>{
-      
-      navigate("/")
-      console.log("this is",response.data,"data")
-     
-    }). catch (error=>{
+    try {
+      const res = await axios.post("http://localhost:3000/api/creatBacket/",formData1);
+      const data = res.json();
+      (response) => {
+        console.log("this is", response.data, "data");
+      };
+    } catch (error) {
       console.error("Error:", error);
-    })
+    }
   };
 
+  // ------------------image-------------
+
+  // axios
+  //   .post("http://localhost:3000/api/product", formData)
+  //   // const data = response.json();
+  //   .then((response) => {
+  //     navigate("/");
+  //     console.log("this is", response.data, "data");
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error:", error);
+  //   });
+
+  // ------------------image-------------
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   // ------------------image-------------
+
+  //   const formData = new FormData();
+  //   formData.append("image", image);
+  //   // ------------------image-------------
+  //   try {
+  //     const res = await axios.post("/upload", formData, {
+  //       headers: {
+  //         "Content-Type": "multipart/form-data",
+  //       },
+  //     });
+
+  //     console.log(res.data, "data not found");
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  //   // ------------------image-------------
+
+  //   // axios
+  //   //   .post("http://localhost:3000/api/product", formData)
+  //   //   // const data = response.json();
+  //   //   .then((response) => {
+  //   //     navigate("/");
+  //   //     console.log("this is", response.data, "data");
+  //   //   })
+  //   //   .catch((error) => {
+  //   //     console.error("Error:", error);
+  //   //   });
+  // };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input
+    <>
+      <form onSubmit={handleSubmit1}>
+        <input
+          type="text"
+          name="bucketName"
+          value={formData1.bucketName}
+          onChange={handleChange1}
+          placeholder="insert bucketName"
+        />
+        <button type="submit">ADD bucket</button>
+      </form>
+
+      {/* <form onSubmit={handleSubmit}>
+        <input
         type="text"
         name="projectName"
         value={formData.projectName}
         onChange={handleChange}
         placeholder="projectName"
       />
-      <input
+        <input
         type="text"
         name="discription"
         value={formData.discription}
@@ -80,77 +120,20 @@ const product = () => {
         placeholder="discription"
       />
 
+        ------------------image-------------
+        <input type="file" onChange={handleImageChange} placeholder="image" />
+        ------------------image-------------
 
-  
-   
-
-       {/* ------------------image------------- */}
-       {/* <input type="file" onChange={handleImageChange} placeholder="image" /> */}
-       {/* ------------------image------------- */}
-
-
-
-
-      <input
+        <input
         type="file"
         name="image"
-       
-        onChange={handleChange}
+        value={formData.image}
+        onChange={handleImageChange}
         placeholder="image"
       />
-      <button type="submit">ADD PRODUCT</button>
-    </form>
-
-    //   <div className="newaddform">
-    //   <form  method="post" action="http://localhost:8080/api/signup/" className="signupform">
-    //     <div className="subdiv">
-    //       <div>
-    //       <label htmlFor="username">userName :</label>
-    //       <input name="username" type="text" id="username" className="inp" />
-    //       </div>
-    //       <div>
-    //       <label htmlFor="name">name :</label>
-    //       <input name="name" type="text" id="name" className="inp" />
-    //       </div>
-    //       <div>
-    //       <label htmlFor="email">Email :</label>
-    //       <input name="email" type="email" id="email" className="inp" />
-    //       </div>
-    //       <div>
-    //       <label htmlFor="city"> city : </label>
-    //       <input name="city" type="text" id="city" className="inp" />
-    //       </div>
-    //     </div>
-    //     <div className="subdiv">
-
-    //       <div>
-    //       <label htmlFor="password" >password :</label>
-    //       <input name="password" type="password" id="password" className="inp" />
-    //       </div>
-    //       <div>
-    //       <label htmlFor="mobile"> Mobile : </label>
-    //       <input name="mobile" type="number" id="mobile" className="inp" />
-    //       </div>
-    //       <div>
-    //       <label htmlFor="area"> area : </label>
-    //       <input name="area" type="text" id="area" className="inp" />
-    //       </div>
-
-    //     </div>
-
-    //     <button className="=btn" type="submit">
-    //       singup
-    //     </button>
-    //   </form>
-
-    //   <div id="btn">
-    //     <form method="get" action="/">
-    //       <button className="=btn" type="submit">
-    //         BACK
-    //       </button>
-    //     </form>
-    //   </div>
-    // </div>
+        <button type="submit">ADD PRODUCT</button>
+      </form> */}
+    </>
   );
 };
 

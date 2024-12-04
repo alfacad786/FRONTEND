965d7f3d1ac836739ef73889ebe6526e99192fa6
alfacad786 <alfa_cad786@yahoo.com>
2024-12-04 12:00:29 +0530

    add timeout in card.jsx

--- src/components/Add_Object.jsx
+++ src/components/Add_Object.jsx
@@ -124,7 +124,7 @@ const Add_Product = () => {
     e.preventDefault();
 
     axios
-      .post(`${HOST_3000}/api/UPDATE`, formData, {
+      .post(`${HOST_3000}/api/UPDATE/`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
